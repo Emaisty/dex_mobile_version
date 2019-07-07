9d36397c8a6b5ab4106c77eca4dd65d3ded4be35
Emaisty <maxgusevfff@yandex.ru>
2019-07-07 16:26:03 +0300

    Initial Commit

---
+++ dex_mobile_version/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dex_mobile_version
+//
+//  Created by Максим Гусев on 07/07/2019.
+//  Copyright © 2019 Максим Гусев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ dex_mobile_versionTests/dex_mobile_versionTests.swift
@@ -0,0 +1,34 @@
+//
+//  dex_mobile_versionTests.swift
+//  dex_mobile_versionTests
+//
+//  Created by Максим Гусев on 07/07/2019.
+//  Copyright © 2019 Максим Гусев. All rights reserved.
+//
+
+import XCTest
+@testable import dex_mobile_version
+
+class dex_mobile_versionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
